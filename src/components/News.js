91c62a15743a5c41500ef86c1368193bc5a74003
Mohit3312 <mohit3312@gmail.com>
2022-11-18 14:42:51 +0530

    Used Fetch API and added Previous & Next Buttons to populate NewsItems

--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,101 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      //   url: "https://newsapi.org/v2/top-headlines?country=in&apiKey=e448d3d02a284a1588ab38b39a1e19f8&category=technology&page=1",
+    };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=e448d3d02a284a1588ab38b39a1e19f8&category=technology&page=1";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handlePrevClick = async () => {
+    console.log("Prev");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e448d3d02a284a1588ab38b39a1e19f8&category=technology&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  handleNextClick = async () => {
+    console.log("Next");
+
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e448d3d02a284a1588ab38b39a1e19f8&category=technology&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
   render() {
     return (
       <div className="container my-3">
-        <h2>GetNews - Top Headlines</h2>
+        <h1>GetNews - Top Headlines</h1>
+
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="myTitle" desc="myDescription" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="myTitle" desc="myDescription" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="myTitle" desc="myDescription" />
-          </div>
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  desc={element.description ? element.description : ""}
+                  imageUrl={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://img.global.news.samsung.com/global/wp-content/uploads/2022/11/Bixby_Availability_Thumb728.jpg"
+                  }
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-dark"
+            disabled={this.state.page <= 1}
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            disabled={this.state.page >= Math.ceil(this.state.totalResults / 20)}
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,21 +2,17 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    let { title, desc } = this.props;
+    let { title, desc, imageUrl, newsUrl } = this.props;
 
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img
-            src="https://images.hindustantimes.com/img/2022/11/17/1600x900/samson_no_1668671104004_1668671109880_1668671109880.JPG"
-            className="card-img-top"
-            alt="..."
-          />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{desc}</p>
-            <a href="/newsDeatil" className="btn btn-primary">
-              Go somewhere
+            <a href={newsUrl} target="_main" className="btn btn-sm btn-primary">
+              Read more
             </a>
           </div>
         </div>
